0a52cd3c61638c4b44b4e335a9d1aee169dd1170
ThomasvanBommel <thomas@vanbommel.ca>
2021-12-29 00:50:36 -0400

    configured cmake

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,12 @@
 // #include <glm/glm.hpp>
 // using namespace glm;
 
+#include <config.h>
 #include <iostream>
 using namespace std;
 
 int main() {
-    cout << "Hello c++\n";
+    cout << "cekeh-client v" << CONFIG_VERSION_MAJOR << "." << CONFIG_VERSION_MINOR << endl;
 
     return 0;
 }
